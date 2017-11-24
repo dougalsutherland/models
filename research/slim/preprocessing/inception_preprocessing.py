1e944a3dd3bec8e84f9dff0b07660440bb853c50
Dougal J. Sutherland <dougal@gmail.com>
2017-11-24 13:51:34 +0000

    slim: lsun dataset

--- research/slim/preprocessing/inception_preprocessing.py
+++ research/slim/preprocessing/inception_preprocessing.py
@@ -241,7 +241,8 @@ def preprocess_for_train(image, height, width, bbox,
 
 
 def preprocess_for_eval(image, height, width,
-                        central_fraction=0.875, scope=None):
+                        central_fraction=None, # XXX: default was 0.875,
+                        scope=None):
   """Prepare one image for evaluation.
 
   If height and width are specified it would output an image with that size by
